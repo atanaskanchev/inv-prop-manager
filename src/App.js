cbad18f6868ba0a75d8c9c8c2f17c4d64052d49f
Atanas Kanchev <atanas.kanchev@river-island.com>
2018-08-11 14:28:25 +0100

    test change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <span>Welcome</span>
+      </div>
+    );
   }
 }
 
