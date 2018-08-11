8a50e6f5b73a18a8ae0eef67430bbe82bb2350c3
Atanas Kanchev <atanas.kanchev@river-island.com>
2018-08-11 19:17:48 +0100

    firebase email sign up working

---
+++ src/components/App/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './index.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import * as routes from '../../constants/routes';
+
+import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navigation />
+
+          <hr />
+
+          <Route
+            exact
+            path={routes.LANDING}
+            component={() => <LandingPage />}
+          />
+          <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
+          <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
+          <Route
+            exact
+            path={routes.PASSWORD_FORGET}
+            component={() => <PasswordForgetPage />}
+          />
+          <Route exact path={routes.HOME} component={() => <HomePage />} />
+          <Route
+            exact
+            path={routes.ACCOUNT}
+            component={() => <AccountPage />}
+          />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/SignUp/index.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import * as routes from '../../constants/routes';
+import { auth } from '../../firebase';
+
+const SignUpPage = () => (
+  <div>
+    <h1>Sign up</h1>
+    <SignUpForm />
+  </div>
+);
+
+const INITIAL_STATE = {
+  username: '',
+  email: '',
+  passwordOne: '',
+  passwordTwo: '',
+  error: null
+};
+
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...INITIAL_STATE };
+  }
+  onSubmit = event => {
+    const { username, email, passwordOne } = this.state;
+    auth
+      .doCreateUserWithEmailAndPassword(email, passwordOne)
+      .then(authUser => {
+        this.setState({ ...INITIAL_STATE });
+      })
+      .catch(error => {
+        this.setState(byPropKey('error', error));
+      });
+
+    event.preventDefault();
+  };
+
+  render() {
+    const { username, email, passwordOne, passwordTwo, error } = this.state;
+
+    const isInvalid =
+      passwordOne !== passwordTwo ||
+      passwordOne === '' ||
+      email === '' ||
+      username === '';
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          value={username}
+          type="text"
+          name="username"
+          onChange={event =>
+            this.setState({ [event.target.name]: event.target.value })
+          }
+          placeholder="Full name"
+          autoComplete="username"
+        />
+        <input
+          value={email}
+          type="email"
+          name="email"
+          onChange={event =>
+            this.setState({ [event.target.name]: event.target.value })
+          }
+          placeholder="email"
+          autoComplete="username"
+        />
+        <input
+          value={passwordOne}
+          type="password"
+          name="passwordOne"
+          onChange={event =>
+            this.setState({ [event.target.name]: event.target.value })
+          }
+          placeholder="password"
+          autoComplete="new-password"
+        />
+        <input
+          value={passwordTwo}
+          type="password"
+          name="passwordTwo"
+          onChange={event =>
+            this.setState({ [event.target.name]: event.target.value })
+          }
+          placeholder="cofirm password"
+          autoComplete="new-password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Sign Up
+        </button>
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+
+const byPropKey = (propertyName, value) => () => ({
+  [propertyName]: value
+});
+
+const SignUpLink = () => (
+  <p>
+    Do you have an account? <Link to={routes.SIGN_UP}>Sign up</Link>
+  </p>
+);
+
+export default withRouter(SignUpPage);
+
+export { SignUpForm, SignUpLink };
