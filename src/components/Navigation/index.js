776cacbfbea07d3594ccff154fe42a04159d3f20
Atanas Kanchev <atanas.kanchev@river-island.com>
2018-08-11 21:19:28 +0100

    sign up shows in non auth state

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -31,6 +31,9 @@ const NavigationNonAuth = () => (
       <Link to={routes.SIGN_IN}>Sign In</Link>
     </li>
     <li>
+      <Link to={routes.SIGN_UP}>Sign Up</Link>
+    </li>
+    <li>
       <Link to={routes.LANDING}>Landing</Link>
     </li>
   </ul>
